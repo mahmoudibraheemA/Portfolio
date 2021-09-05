93b91b2c9802e2e9cdb393e08aa224ddec8b3250
mahmoud ibraheem <ninjathe85@yahoo.com>
2021-09-05 20:32:45 +0200

    updated

---
+++ Infrastructure/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Infrastructure
+{
+    class DataContext:DbContext
+    {
+        //defuatl constructor
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+
+        }
+        
+    }
+
+}
