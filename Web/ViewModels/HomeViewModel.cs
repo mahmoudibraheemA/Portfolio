00e4ef0df87c5b85f72316e39f2ec570385a3620
mahmoud ibraheem <ninjathe85@yahoo.com>
2021-09-06 20:43:11 +0200

    updated

---
+++ Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public Owner owner { get; set; }
+        public List<Portfolio> portfolio { get; set; }
+    }
+}
