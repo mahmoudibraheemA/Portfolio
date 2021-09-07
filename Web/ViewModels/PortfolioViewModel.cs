c1a23de7ec17c176b9e7c926a178cb81491d86e9
mahmoud ibraheem <ninjathe85@yahoo.com>
2021-09-07 18:08:20 +0200

    updated

---
+++ Web/ViewModels/PortfolioViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.ViewModels
+{
+    public class PortfolioViewModel
+    {
+        public Guid ID { get; set; }
+        public string Projectname { get; set; }
+        public string Decsription { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile file { get; set; }
+    }
+}
